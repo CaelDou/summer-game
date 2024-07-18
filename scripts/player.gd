0acf34bb61da2639eb7521107157f7513e31f882
Chris7962 <cristianspigoloncs@gmail.com>
2024-07-17 23:20:31 -0300

    Idle and walk animations
    Added idle and walk animations logic. Current animations are simply placeholders. Also readjusted movement code and removed acceleration because diagonal movement was weird, but we can add it back if someone wants it

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,48 @@
 extends CharacterBody2D
 
+# reference to the AnimatedSprite2D node
+@onready var anim = $AnimatedSprite2D
+
+# =====< movement >=====
 @export var speed: float = 100 # max speed
 @export var accel: float = 10  # how quickly the player get to max speed
+var direction: Vector2 = Vector2.ZERO
+var last_direction: Vector2 = Vector2.ZERO
+
+# =====< animation >=====
+var direction_anim = {
+	Vector2(0, -1): "up",
+	Vector2(0, 0): "down", # last_direction is (0, 0) on load
+	Vector2(0, 1): "down",
+	Vector2(1, 0): "right",
+	Vector2(1, 1): "right",
+	Vector2(1, -1): "right", 
+	Vector2(-1, 0): "left",
+	Vector2(-1, 1): "left",
+	Vector2(-1, -1): "left"
+}
+
+func _process(_delta):
+	# update last_direction until the player stops
+	if direction != Vector2.ZERO:
+		last_direction = direction
+	
+	# check the direction to handle animations
+	if direction == Vector2.ZERO:
+		anim.play("idle_" + direction_anim[last_direction])
+	else:
+		anim.play("walk_" + direction_anim[direction])
 
 func _physics_process(_delta):
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	# get the input direction in a Vector2, so left would be (-1, 0)
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
-	velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+	# handle movement
+	if direction != Vector2.ZERO:
+		# normalize so the player doesn't go faster diagonally
+		velocity = direction.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
 	
 	move_and_slide()
